895e5c73f8e8d3d3f65ea85bf1cadef838f33c55
fomineandrei <fomineandrei@yandex.ru>
2025-06-09 11:06:10 +0300

    refactored page_analyzer/app.py

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -3,12 +3,16 @@ import os
 from dotenv import load_dotenv
 from flask import Flask
 
-load_dotenv()
-app = Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+def create_app():
+    load_dotenv()
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+    from page_analyzer.views import index
+    app.register_blueprint(index)
+
+    return app
 
 
-@app.route('/')
-def main_page():
-    return "Любимая мамочка, я тебя очень сильно люблю." + \
-        "Что хочешь на ужин в пятницу?)))"
+app = create_app()
