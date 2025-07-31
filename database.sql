6f26c130ecb61f45419d811c690dcaa8b9dccfd7
fomineandrei <fomineandrei@yandex.ru>
2025-07-31 14:08:23 +0300

    fixed database.sql

--- database.sql
+++ database.sql
@@ -1,15 +1,17 @@
 CREATE TABLE IF NOT EXISTS urls (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL UNIQUE,
-    created_at TIMESTAMP
+    created_at TIMESTAMP NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS url_check (
+CREATE TABLE IF NOT EXISTS url_checks (
     id SERIAL PRIMARY KEY,
     url_id INTEGER REFERENCES urls(id) ON DELETE CASCADE NOT NULL,
     status_code INTEGER,
     h1 VARCHAR(255),
     title VARCHAR(255),
     description TEXT,
-    check_date TIMESTAMP NOT NULL
+    created_at TIMESTAMP NOT NULL
 );
+
+DROP TABLE IF EXISTS url_check;
